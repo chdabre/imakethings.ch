dabcff224e75bbcbee9e8e46108f1e061dc94778
Dario Breitenstein <online@imakethings.ch>
2020-05-18 20:31:17 +0200

    add yt embed

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -12,6 +12,13 @@ module.exports = {
                     useLinkSyntax: false
                 }
             })
+
+            md.use(require('markdown-it-video', {
+                youtube: { width: 640, height: 390 },
+                vimeo: { width: 500, height: 281 },
+                vine: { width: 600, height: 600, embed: 'simple' },
+                prezi: { width: 550, height: 400 }
+            }));
         }
     },
 }
