972a134b94dddfe4458471e994cab09dc5085821
Dario Breitenstein <online@imakethings.ch>
2020-05-19 09:21:00 +0200

    add favicons

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -4,6 +4,17 @@ module.exports = {
     themeConfig: {
         cloudinary_base_url: "https://res.cloudinary.com/dey9hupke/image/upload"
     },
+    head: [
+        ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
+        ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
+        ['link', { rel: "manifest", href: "/site.webmanifest"}],
+        ['link', { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#E5E5E5"}],
+        ['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+        ['meta', { name: "msapplication-TileColor", content: "#603cba"}],
+        ['meta', { name: "msapplication-config", content: "/browserconfig.xml"}],
+        ['meta', { name: "theme-color", content: "#E5E5E5"}],
+    ],
     markdown: {
         extendMarkdown: md => {
             md.use(require('markdown-it-html5-embed'), {
